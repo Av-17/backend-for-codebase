990cb4eaeb7be65f9bbb981f0fc7dd27d12ed5e4
Avdhesh <avdheshpr17@gmail.com>
2025-07-25 16:12:17 +0530

    added demo

--- demo.py
+++ demo.py
@@ -74,4 +74,4 @@ def callback(code: str):
     # res = RedirectResponse(url=f"http://localhost:8501/login?token={token}&username={username}")  # <-- redirect to app.py
     # res.set_cookie(key="access_token", value=token, max_age=86400, path="/")
     # res.set_cookie(key="username", value=username, max_age=86400, path="/")
-    return RedirectResponse(url=f"https://your-streamlit-app-name.streamlit.app/?token={token}&username={username}") 
+    return RedirectResponse(url=f"https://9smyej28fvnzpnw8fyqhyo.streamlit.app/?token={token}&username={username}")
